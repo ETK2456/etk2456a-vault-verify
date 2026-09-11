c2cfbbe50d9ca04ae2b13f0583e777597e64bc88
ETK Dev <regaloamira0@gmail.com>
2026-09-11 15:54:46 +0100

    Update service worker to version 3 and improve caching

--- sw.js
+++ sw.js
@@ -1,18 +1,31 @@
-// TJ3 Vault - Offline Service Worker - Fix blob error
-const CACHE='tj3-vault-v2';
-const URLS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)));
+// TJ3 Vault - Offline Service Worker - v3 - FIX 404 + force update
+const CACHE = 'tj3-vault-v3';
+const URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
   self.skipWaiting();
 });
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{
-    if(r.ok)caches.open(CACHE).then(ca=>ca.put(e.request,r.clone()));
-    return r;
-  }).catch(()=>c||caches.match('./index.html'))));
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        if (res.ok) {
+          caches.open(CACHE).then(cache => cache.put(e.request, res.clone()));
+        }
+        return res;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
 });
